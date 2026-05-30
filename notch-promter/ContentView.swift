5d46f0ed182d4feb32d7df171e36eb8dfc42ae2e
eba <erdincburak.arslan@gmail.com>
2026-05-30 08:56:36 +0300

    Initial Commit

---
+++ notch-promter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  notch-promter
+//
+//  Created by Erdinc Burak ARSLAN on 30.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ notch-promter/notch_promterApp.swift
@@ -0,0 +1,17 @@
+//
+//  notch_promterApp.swift
+//  notch-promter
+//
+//  Created by Erdinc Burak ARSLAN on 30.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct notch_promterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
